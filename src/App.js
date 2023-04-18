9a26d5e24ae33b34a643e197e874f3c6f1e2ccb6
layfr <laylafirsova@gmail.com>
2023-04-18 13:58:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { AppRouter } from './components/AppRouter';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
+  );
+}
+
+export default App;
